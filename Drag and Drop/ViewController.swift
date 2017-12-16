dcd086833d79b88d6d1182e73f5e1ca809e707f9
akilkumar28 <akil.psn@gmail.com>
2017-12-16 12:05:55 -0700

    drag implementation completed
    droppping implementation is still left.

--- Drag and Drop/ViewController.swift
+++ Drag and Drop/ViewController.swift
@@ -7,19 +7,89 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,UITableViewDragDelegate,UITableViewDropDelegate {
+    
 
+    let leftTableView = UITableView()
+    let rightTableView = UITableView()
+    
+    var leftArray  = [String](repeating: "left", count: 10)
+    var rightArray = [String](repeating: "right", count: 10)
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        intianSetup()
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    fileprivate func intianSetup() {
+        leftTableView.delegate = self
+        rightTableView.delegate = self
+        
+        leftTableView.dataSource = self
+        rightTableView.dataSource = self
+        
+        leftTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        rightTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        leftTableView.frame = CGRect(x: 0, y: 50, width: 150, height: 400)
+        rightTableView.frame = CGRect(x: 150, y: 50, width: 150, height: 400)
+        
+        leftTableView.dragDelegate = self
+        rightTableView.dragDelegate = self
+        
+        leftTableView.dropDelegate = self
+        rightTableView.dropDelegate = self
+        
+        leftTableView.dragInteractionEnabled = true
+        rightTableView.dragInteractionEnabled = true
+        
+        
+        self.view.addSubview(leftTableView)
+        self.view.addSubview(rightTableView)
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == leftTableView {
+            return leftArray.count
+        }
+        return rightArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else {return UITableViewCell()}
+        
+        if tableView == leftTableView {
+            cell.textLabel?.text = leftArray[indexPath.row]
+        }else{
+            cell.textLabel?.text = rightArray[indexPath.row]
+            
+        }
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        let item:String!
+        if tableView == leftTableView {
+            item = leftArray[indexPath.row]
+        }else{
+            item = rightArray[indexPath.row]
+        }
+        guard let itemData = item.data(using: .utf8) else {return []}
+        let itemProvider = NSItemProvider(item: itemData as NSData, typeIdentifier: kUTTypePlainText as String)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        return [dragItem]
+    }
+    
+    func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
+        print(222)
+    }
 }
 
